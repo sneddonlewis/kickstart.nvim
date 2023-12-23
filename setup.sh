4cdfd54f8b2493e5f1e09c71a1802e811fd8d16e
Lewis Sneddon <lewis@lewiscodes.io>
2023-12-23 13:28:17 +0000

    Modified setup script to invoke lazy in nvim

--- setup.sh
+++ setup.sh
@@ -4,5 +4,9 @@ nvim_config="${XDG_CONFIG_HOME:-$HOME/.config}/nvim"
 
 rm -rf "$nvim_config"
 
-cp -R . "$nvim_config"
+mkdir -p "$nvim_config"
+
+cp -R ./* "$nvim_config"
+
+nvim --headless "+Lazy! sync" +qa
 
